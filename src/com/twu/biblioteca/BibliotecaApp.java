72ce6a61f90af646e15312d595eb1930a733a2e1
vanbujm <vanbujm@gmail.com>
2016-08-01 23:02:24 +0800

    Added welcome message to main

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,9 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        System.out.print(welcome());
     }
 
-    String welcome() {
+    static String welcome() {
         return "Welcome to Biblioteca!";
     }
 }
--- test/com/twu/biblioteca/TestBibliotecaApp.java
+++ test/com/twu/biblioteca/TestBibliotecaApp.java
@@ -1,13 +1,36 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestBibliotecaApp {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
 
     @Test
     public void testWelcomeMessageSaysWelcome() {
-        assertEquals("Welcome to Biblioteca!",new BibliotecaApp().welcome());
+        assertEquals("Welcome to Biblioteca!", BibliotecaApp.welcome());
+    }
+
+    @Test
+    public void testMainSaysWelcome() {
+        BibliotecaApp.main(new String[] {});
+        assertEquals("Welcome to Biblioteca!", outContent.toString());
     }
 }
